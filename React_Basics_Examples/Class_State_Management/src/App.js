e215f0b2e926e5631ada9a54e7dd6837f7dd44f4
Chandrakant Hatti <chandusayhi@users.noreply.github.com>
2022-11-24 15:02:58 +0530

    Create App.js

---
+++ React_Basics_Examples/Class_State_Management/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Employee from './StateManagement/ClassBased_StateManage';
+
+
+
+
+
+
+
+
+function App() {
+  
+
+  return(
+    <React.Fragment>
+      <Employee/>
+    </React.Fragment>
+  );
+    
+
+
+
+  };
+
+export default App;
